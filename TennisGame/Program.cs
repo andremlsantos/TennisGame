22d9b9cdf597c54d2b1e845dc3e2b20a060fa370
Andre Santos <andremlsantos@gmail.com>
2021-10-10 22:42:50 +0200

    Initial commit on Tests

---
+++ TennisGame/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TennisGame
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var tennis = new TennisGame1(new Player("1"), new Player("2"));
+            var score = tennis.GetScore();
+        }
+    }
+}
---
+++ Tests/TennisTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace TennisTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
